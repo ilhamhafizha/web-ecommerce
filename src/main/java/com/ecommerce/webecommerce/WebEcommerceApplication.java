09655fcb315c7268b959f6abefb020e4893a7c52
muhammadilham <muhammadilhamhafizha@gmail.com>
2026-01-05 14:56:47 +0700

    feat: setting springboot

---
+++ src/main/java/com/ecommerce/webecommerce/WebEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.webecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/webecommerce/WebEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.webecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
